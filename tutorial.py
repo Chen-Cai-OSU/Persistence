a7568b27e838cf95cc2b641944e49c7806372634
Chen-Cai-OSU <47577816+Chen-Cai-OSU@users.noreply.github.com>
2020-05-26 13:02:22 -0400

    add visualization of clustering tree

--- tutorial.py
+++ tutorial.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[13]:
 
 
 import matplotlib.pyplot as plt
@@ -11,7 +11,7 @@ from sklearn.cluster import AgglomerativeClustering
 np.random.seed(42)
 
 
-# In[2]:
+# In[14]:
 
 
 # some helper functions. Feel free to modify them.
@@ -57,6 +57,10 @@ def cluster(X):
 
 
 # # Generate persistence diagrams
+
+# In[15]:
+
+
 # generate 40 persistence diagrams. Note that I delibrately generate two types of diagrams that are quite different.
 diags = [np.random.random((10, 2)) for _ in range(20)]
 diags += [1 + np.random.random((10, 2)) for _ in range(20)]
@@ -68,26 +72,105 @@ plt.imshow(np.flip(np.reshape(pi[0], [100,100]), 0))
 plt.title("Persistence Image")
 plt.show()
 
+# plot the origianal diagram
+plt.scatter(diags[0][:,0],diags[0][:,1])
+plt.plot([0.,1.],[0.,1.])
+plt.title("Persistence Diagram")
+plt.show()
+
 
 # # Compute kernel for persistence diagrams
+
+# In[16]:
+
+
 # compute sliced wasserstein kernels
 kernel = sw_kernel(diags, diags)
 plot_kernel(kernel, title='Kernel')
 
 
 # # Convert Kernel to Distance Matrix
+
+# In[17]:
+
+
 # plot distance matrix
 dist = kernel2dist(kernel)
 plot_kernel(dist, title='Distance Matrix')
 
 
 # # Cluster Persistence Diagrams
+
+# In[18]:
+
+
 # cluster and print out labels. Since we knew there are two "types" of persistence diagrams by construction,
 # we expect that clustering algorithm can differentiate them, which is indeed the case!
 labels = cluster(dist)
 print(labels)
 
 
+# # Visualize Clustering Tree 
+
+# In[19]:
+
+
+# modified from https://scikit-learn.org/stable/auto_examples/cluster/plot_agglomerative_dendrogram.html#sphx-glr-auto-examples-cluster-plot-agglomerative-dendrogram-py
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.datasets import load_iris
+from sklearn.cluster import AgglomerativeClustering
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+X = dist 
+# setting distance_threshold=0 ensures we compute the full tree.
+model = AgglomerativeClustering(affinity='precomputed', linkage='average', distance_threshold=0, n_clusters=None)
+# AgglomerativeClustering(distance_threshold=0, n_clusters=None)
+
+model = model.fit(X)
+plt.title('Hierarchical Clustering Dendrogram')
+# plot the top three levels of the dendrogram
+plot_dendrogram(model, truncate_mode='level', p=3)
+plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
 
 
 
